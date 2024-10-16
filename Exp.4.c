907d01c9a817e7f5aeef0e9a3dcf9048d3f82717
SKJha-99 <sakshamjha2006@gmail.com>
2024-10-16 23:24:39 +0530

    Update Exp.4.c

--- Exp.4.c
+++ Exp.4.c
@@ -1,1 +1,41 @@
 
+/*
+Exp No.:4
+Title :WAP to find the sum of all the odd numbers between numbers entered by the user.
+Name:Saksham Jha
+Class:First Year
+Div:C
+UIN:241P067
+Roll No.:061
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int i,strt,end,sum=0;
+
+    printf("Enter the Starting no. :");
+    scanf("%d",&strt);
+    printf("Enter the End no. :");
+    scanf("%d",&end);
+
+    for(i=strt;i<=end;i++)
+    {
+        if(i%2!=0)
+        {
+            sum=i+sum;
+        }
+    }
+    printf("Addition of ODD numbers is %d",sum);
+
+    return 0;
+}
+
+
+OUTPUT:
+Enter the Starting no. :0
+Enter the End no. :35
+Addition of ODD numbers is 324
+Process returned 0 (0x0)   execution time : 9.312 s
+Press any key to continue.
