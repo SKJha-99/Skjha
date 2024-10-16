190f162d7f3a88f708f6a947e1ff1783e1cba8c3
SKJha-99 <sakshamjha2006@gmail.com>
2024-10-16 23:27:40 +0530

    Create Exp.1.C

---
+++ Exp.1.C
@@ -0,0 +1,35 @@
+
+/*
+Exp No.:1
+Title :WAP to print your name,age,class,division,and UIN on the screen. Use escape sequence to properly format the output
+Name:Saksham Jha
+Class:First Year
+Div:C
+UIN:241P067
+Roll No.:061
+*/
+
+
+#include<stdio.h>
+
+int main()
+{
+    printf("NAME\t:\tSaksham Jha\n");
+    printf("AGE\t:\t17\n");
+    printf("CLASS\t:\tFirst Year\n");
+    printf("DIV\t:\tC\n");
+    printf("UIN\t:\t241P067\n");
+
+    return 0;
+}
+
+
+OUTPUT: 
+NAME    :       Saksham Jha
+AGE     :       17
+CLASS   :       First Year
+DIV     :       C
+UIN     :       241P067
+
+Process returned 0 (0x0)   execution time : 1.038 s
+Press any key to continue.
